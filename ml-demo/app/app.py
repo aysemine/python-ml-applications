5f5dc80183e770656a834bcd203b750f768ed528
aysemine <ayseminerolur@gmail.com>
2025-09-25 13:41:12 +0300

    try fix port issue

--- ml-demo/app/app.py
+++ ml-demo/app/app.py
@@ -3,9 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
 import joblib
+import os
+import uvicorn
 
 app = FastAPI()
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
 origins = ["*"]
 
 app.add_middleware(
